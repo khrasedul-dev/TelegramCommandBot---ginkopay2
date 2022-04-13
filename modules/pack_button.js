109530d2f30442a2c7f759df677e75c9e1f39ba3
KH Rasedul Islam <87899184+khrasedul-dev@users.noreply.github.com>
2022-04-13 22:39:49 +0600

    Update pack_button.js

--- modules/pack_button.js
+++ modules/pack_button.js
@@ -8,7 +8,7 @@ const silver = (ctx)=>{
 
 const gold = (ctx)=>{
 
-    ctx.telegram.sendMessage( ctx.chat.id , `Start 50.00 € \nRewards 10% per year \nPayment every 6 months`).catch((e)=>{
+    ctx.telegram.sendMessage( ctx.chat.id , `Start to € 50,000 € \nRewards 10% per year \nPayment every 6 months`).catch((e)=>{
         console.log(e)
         ctx.reply("Something is wrong")
     })
